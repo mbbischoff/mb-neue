c8d37fa83d09ba8bbbb61e7ddfe4d5fcb68be2d6
mb bischoff <mb@mbbischoff.com>
2025-04-13 12:30:18 -0400

    hide in header

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,11 @@ export interface NavItem {
   href: string;
   label: string;
   icon?: string;
+  hideInHeader?: boolean;
 }
 
 export const navigation: NavItem[] = [
-  { href: '/', label: '⌂︎ Home' },
+  { href: '/', label: '⌂︎ Home', hideInHeader: true },
   { href: '/now', label: '⌚︎ Now' },
   { href: '/archive', label: '⊞︎ Then' },
   { href: '/poems', label: '⁂ Poems' },
